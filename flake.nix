9893f515e3c878316d4e9d356813a253cd79c493
Bernardo Meurer <bernardo@meurer.org>
2021-04-01 18:25:21 -0700

    treewide: init

---
+++ flake.nix
@@ -0,0 +1,38 @@
+{
+  description = "lovesegfault's CV";
+
+  inputs = {
+    flake-utils.url = "github:numtide/flake-utils";
+    gitignore = {
+      url = "github:hercules-ci/gitignore.nix";
+      flake = false;
+    };
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable-small";
+  };
+
+  outputs = { self, flake-utils, gitignore, nixpkgs, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+        gitignoreSource = (import gitignore { inherit (pkgs) lib; }).gitignoreSource;
+      in
+      {
+        defaultPackage = with pkgs; stdenv.mkDerivation {
+          name = "blog";
+          src = gitignoreSource ./.;
+
+          nativeBuildInputs = [ zola ];
+
+          makeFlags = [ "PREFIX=${placeholder "out"}" ];
+        };
+
+        devShell = self.defaultPackage.${system}.overrideAttrs (oldAttrs: {
+          buildInputs = with pkgs; (oldAttrs.buildInputs or [ ]) ++ [
+            git-lfs
+            hugo
+            mdl
+            proselint
+          ];
+        });
+      });
+}
